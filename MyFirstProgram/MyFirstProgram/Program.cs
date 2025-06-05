086f5678abb87d60c8d92fe944a5fedd4907edf4
Faaeq Fazal <faaeqrt@gmail.com>
2025-06-05 16:53:29 +0530

    Started with the basic fo c-sharp

---
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started to learn C#");
+            Console.Beep();
+        }
+    }
+}
