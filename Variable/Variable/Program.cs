28c3115864bc9bebe9f451b675590b536c8893a6
Faaeq Fazal <faaeqrt@gmail.com>
2025-06-06 23:46:21 +0530

    Proper variable declaration and using console to practice and check whether its working

--- Variable/Variable/Program.cs
+++ Variable/Variable/Program.cs
@@ -10,6 +10,43 @@ namespace Variable
     {
         static void Main(string[] args)
         {
+            // Declaring a variable
+            int x;
+            x = 123; // Intializing
+
+            int y = 321;
+
+            int z = x + y;
+
+            int age = 21;
+
+            double height = 300.5;
+
+            bool alive = false;
+
+            char symbol = '@';
+
+            String name = "Mohamed";
+
+            String username = symbol + name;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+            Console.WriteLine("Hello " + name);
+
+            Console.WriteLine("Your age is " + age);
+
+            Console.WriteLine("Your height is " + height + "cm");
+
+            Console.WriteLine("Are you alive? " + alive);
+
+            Console.WriteLine("Your symbol is: " + symbol);
+
+            Console.WriteLine("Your username is: " + username);
+
+            Console.ReadKey();
         }
     }
 }
