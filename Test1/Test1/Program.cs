bb2a52f0e0ab2f7ea1f2f8e5be8e0e18d27ca011
Faaeq Fazal <faaeqrt@gmail.com>
2025-06-07 00:25:30 +0530

    Test 1: Accepting user inputs

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Accepting user inputs
+
+            Console.WriteLine("What's your name? ");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("What's your age? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Your name is " + name + " and your age is " + age + " years old.");
+
+            Console.ReadKey();
+        }
+    }
+}
