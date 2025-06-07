5dd42e8a301804af73eb96ec39908940d3c1139f
Faaeq Fazal <faaeqrt@gmail.com>
2025-06-07 11:14:46 +0530

    Arithmetic operators and use of math class

---
+++ ArithmeticOperators/ArithmeticOperators/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+
+            // Addition
+            //num = num + 2;
+            //num += 2;
+            //num++;
+
+            // Subtraction
+            //num = num - 1;
+            //num -= 1;
+            //num--;
+
+            // Multiplication
+            //num = num * 2;
+            //num *= 2;
+
+            // Division
+            //num = num / 2;
+            //num /= 2;
+
+            int remainder = num % 3;
+
+            Console.WriteLine("The value stored in num is " + num);
+
+            Console.WriteLine("The remainder value is " + remainder);
+
+            // Use of the math class
+            double x = 3;
+            double y = 5;
+            
+            // Calculating the power
+            double a = Math.Pow(x, 2);
+            Console.WriteLine("Power " + a);
+
+            // Calculating the square root
+            double b = Math.Sqrt(x);
+            Console.WriteLine("Square Root " + b);
+
+            // The absolute value of -3 is +3
+            double x1 = -3;
+            double c = Math.Abs(x1);
+            Console.WriteLine("Absolute " + c);
+
+            // The rounded value
+            double x2 = 3.51;
+            double d = Math.Round(x2);
+            Console.WriteLine("Rounded value " + d);
+
+            // Round up
+            double e = Math.Ceiling(x2);
+            Console.WriteLine("Round up " + e);
+
+            // Round down
+            double f = Math.Floor(x2);
+            Console.WriteLine("Round down " + f);
+
+            // Finding the maximum value
+            double g = Math.Max(x, y);
+            Console.WriteLine("Maximum value " + g);
+
+            // Finding the minimum value
+            double h = Math.Min(x, y);
+            Console.WriteLine("Minimum value " + h);
+
+            // Generating a random numbers
+            Random random = new Random();
+            // Guessing rolling a dice
+            int num1 = random.Next(1, 7);
+            Console.WriteLine(num1);
+
+            double num2 = random.NextDouble();
+            Console.WriteLine(num2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ImutableV/ImutableV/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ImutableV")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ImutableV")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ce04d94d-683d-4ca6-82c1-a45e35d5889d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
