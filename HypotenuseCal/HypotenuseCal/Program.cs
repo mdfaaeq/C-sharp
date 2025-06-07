991489f6444debd7a7566b91f1218e09f3bd50d4
Faaeq Fazal <faaeqrt@gmail.com>
2025-06-07 11:28:40 +0530

    A small calculation performing

---
+++ HypotenuseCal/HypotenuseCal/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HypotenuseCal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter side a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter side b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            Console.WriteLine("The hypotenuse is: " + c);
+
+            Console.ReadKey();
+        }
+    }
+}
