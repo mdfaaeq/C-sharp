0b36e4cd021290bcbdd94583bddd1e8873c60f9f
Faaeq Fazal <faaeqrt@gmail.com>
2025-06-09 21:15:56 +0530

    using conditional operators

---
+++ ConditionalOperators/ConditionalOperators/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double temperature = 20;
+            String message;
+            String message1;
+
+            // Performance using if condition
+            if (temperature >= 15)
+            {
+                message1 = "It's warm outside";
+            }
+            else
+            {
+                message1 = "It's cold outside";
+            }
+            Console.WriteLine(message1);
+
+            // Performance using condition operator (condition) ? x : y
+            // Since its return a value it needs to be assigned
+            message = (temperature >= 15) ? "It's warm outside" : "It's cold outside";
+            Console.WriteLine(message);
+
+            Console.ReadKey();
+
+        }
+    }
+}
