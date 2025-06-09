d3d524a707822b8c6c5d48f5ec3ab740038515e3
Faaeq Fazal <faaeqrt@gmail.com>
2025-06-09 12:09:36 +0530

    Used a nested loop to print a rechtrangle.

---
+++ NestedLoop/NestedLoop/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Drawing a rechtangle using any symbols
+            // Outer loop looks after the number of rows and the inner loop will be focusing on columns.
+
+            Console.Write("How many rows? ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("How many columns? ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Whaty symbol: ");
+            String symbol = Console.ReadLine();
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }   
+    }
+}
