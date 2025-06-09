0446d02033b9cb0178112a83a23acd8b162e4a97
Faaeq Fazal <faaeqrt@gmail.com>
2025-06-09 19:28:37 +0530

    proper use of methods

---
+++ methods/methods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "Daily Routing";
+
+            message(msg);
+
+            Console.ReadKey();
+        }
+
+        static void message(string msg) {
+            Console.WriteLine(msg);
+            Console.WriteLine("1. Wake up");
+            Console.WriteLine("2. Eat");
+            Console.WriteLine("3. Code");
+            Console.WriteLine("4. Sleep");
+            Console.WriteLine();
+        }
+    }
+}
