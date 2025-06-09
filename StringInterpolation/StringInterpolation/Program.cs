3140687ea5b7c22eb66fb4369ff8d59c9cdb09f9
Faaeq Fazal <faaeqrt@gmail.com>
2025-06-09 21:28:10 +0530

    Inserting variables into a string literal

---
+++ StringInterpolation/StringInterpolation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringInterpolation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // String interpolation allows embedding variables directly inside strings using the $ symbol.
+            // Example: $"Result: {result}" is easier to read and less error-prone than "Result: " + result.
+
+            string firstName = "Mohamed";
+            string lastName = "Fazal";
+            int age = 22;
+
+            //Console.WriteLine("Hello " + firstName + " " + lastName);
+            //Console.WriteLine("Your are " + age + " years old.");
+            //Console.WriteLine();
+
+            Console.WriteLine($"Hello {firstName} {lastName}");
+            Console.WriteLine($"Your are {age} years old.");
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
