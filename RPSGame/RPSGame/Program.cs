6ee4fb665396ce85aa51021ffc5e360fbafd75ed
Faaeq Fazal <faaeqrt@gmail.com>
2025-06-09 18:35:26 +0530

    Created a rock, paper,  scissors game

---
+++ RPSGame/RPSGame/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            bool playAgain = true;
+            string player;
+            string computer;
+            string response;
+
+            while (playAgain) {
+
+                player = "";
+                computer = "";
+
+                while (player != "ROCK" && player != "PAPER" && player != "SCISSORS") {
+                    Console.Write("Enter ROCK, PAPER, or SCISSORS: ");
+                    player = Console.ReadLine();
+                    player = player.ToUpper();
+                }
+
+                switch (random.Next(1, 4)) { 
+                    case 1:
+                        computer = "ROCK";
+                        break;
+                    case 2:
+                        computer = "PAPER";
+                        break;
+                     case 3:
+                        computer = "SCISSORS";
+                        break;
+                }
+                Console.WriteLine("Player: " + player);
+                Console.WriteLine("Computer: " + computer);
+
+                switch (player) {
+                    case "ROCK":
+                        if (computer == "ROCK")
+                        {
+                            Console.WriteLine("It's a draw!");
+                        } else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("You lose!");
+                        } else
+                        {
+                            Console.WriteLine("You win!");
+                        }
+                            break;
+                    case "PAPER":
+                        if (computer == "PAPER")
+                        {
+                            Console.WriteLine("It's a draw!");
+                        }
+                        else if (computer == "ROCK")
+                        {
+                            Console.WriteLine("You win!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You lose!");
+                        }
+                        break;
+                    case "SCISSORS":
+                        if (computer == "SCISSORS")
+                        {
+                            Console.WriteLine("It's a draw!");
+                        } else if (computer == "ROCK")
+                        {
+                            Console.WriteLine("You lose!");
+                        } else
+                        {
+                            Console.WriteLine("You win!");
+                        }
+                        break;
+                }
+
+                Console.Write("Would you like to play again (Y/N): ");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if (response == "Y")
+                {
+                    playAgain = true;
+                }
+                else if (response == "N")
+                {
+                    playAgain = false;
+                }
+                else {
+                    Console.WriteLine("Enter (Y/N) to continue.");
+                }
+            }
+
+            Console.WriteLine("Thank you for playing...");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
