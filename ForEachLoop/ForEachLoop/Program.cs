f1c632719209f17dfe74fbd9850906b997bc3d9c
Faaeq Fazal <faaeqrt@gmail.com>
2025-06-09 19:16:57 +0530

    For each loop

---
+++ ForEachLoop/ForEachLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] cars = { "BMW", "Toyota", "BYD" };
+
+            foreach (String car in cars)
+            {
+                Console.WriteLine(car);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
