911eac70154292dfea8c93ef75f69632e456bbed
Faaeq Fazal <faaeqrt@gmail.com>
2025-06-10 20:09:07 +0530

    Learnt properly how to pass objects as arguments and method overriding

---
+++ Arguments/Arguments/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arguments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("BMW", "Black");
+            Car car2 = Copy(car1);
+
+            ChangeColor(car1, "Blue");
+
+            Console.WriteLine(car1.color + " " + car1.model);
+            Console.WriteLine(car2.color + " " + car2.model);
+
+            Console.ReadKey();
+        }
+
+        public static void ChangeColor(Car car, string color)
+        {
+            car.color = color;
+        }
+
+        public static Car Copy(Car car)
+        {
+            return new Car(car.model, car.color);
+        }
+    }
+
+    class Car
+    {
+        public string model;
+        public string color;
+        public Car(string model, string color)
+        {
+            this.model = model;
+            this.color = color;
+        }
+    }
+}
+
---
+++ MethodOverriding/MethodOverriding/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            dog.speak();
+            cat.speak();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Animal
+    {
+        public virtual void speak()
+        {
+            Console.WriteLine("The animal goes *brrr*");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void speak()
+        {
+            Console.WriteLine("The dogs goes *woof*");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void speak()
+        {
+            Console.WriteLine("The cat goes *meow*");
+        }
+    }
+}
