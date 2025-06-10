a6fd7062a5b931281f3901a61ef223698803bb71
Faaeq Fazal <faaeqrt@gmail.com>
2025-06-10 23:48:29 +0530

    Created a list of objects and practiced

---
+++ ListOfObjects/ListOfObjects/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOfObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Player> players = new List<Player>();
+
+            players.Add(new Player("Mohamed"));
+            players.Add(new Player("Hatim"));
+            players.Add(new Player("Tai"));
+
+            foreach (Player p in players) {
+                Console.WriteLine(p);
+                // Console.WriteLine(p.username);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Player
+    {
+        public string username;
+
+        public Player(string username)
+        {
+            this.username = username;
+        }
+
+        public override string ToString()
+        {
+            return username;
+        }
+    }
+}
