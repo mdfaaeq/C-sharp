b6293ba84ae30272230bc258e89c4e91dead6001
Faaeq Fazal <faaeqrt@gmail.com>
2025-06-10 16:02:39 +0530

    Properly implemented abstraction

--- AbstractClass/AbstractClass/Program.cs
+++ AbstractClass/AbstractClass/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AbstractClass
 {
@@ -10,6 +6,75 @@ namespace AbstractClass
     {
         static void Main(string[] args)
         {
+            Vehicle car = new Car();
+            Vehicle bicycle = new Bicycle();
+            Vehicle boat = new Boat();
+
+            DisplayVehicle(car);
+            DisplayVehicle(bicycle);
+            DisplayVehicle(boat);
+
+            Console.ReadKey();
+        }
+
+        static void DisplayVehicle(Vehicle vehicle)
+        {
+            Console.WriteLine("The speed is: " + vehicle.Speed);
+            Console.WriteLine("The number of wheels is: " + vehicle.Wheels);
+            vehicle.go();
+            Console.WriteLine();
+        }
+    }
+
+    abstract class Vehicle
+    {
+        public int Speed { get; set; }
+        public abstract int Wheels { get; }
+        public abstract void go();
+    }
+
+    class Car : Vehicle
+    {
+        public override int Wheels => 4;
+
+        public Car()
+        {
+            Speed = 120;
+        }
+
+        public override void go()
+        {
+            Console.WriteLine("The car is driving!");
+        }
+    }
+
+    class Bicycle : Vehicle
+    {
+        public override int Wheels => 2;
+
+        public Bicycle()
+        {
+            Speed = 20;
+        }
+
+        public override void go()
+        {
+            Console.WriteLine("The bicycle is pedaling!");
+        }
+    }
+
+    class Boat : Vehicle
+    {
+        public override int Wheels => 0;
+
+        public Boat()
+        {
+            Speed = 30;
+        }
+
+        public override void go()
+        {
+            Console.WriteLine("The boat is sailing!");
         }
     }
 }
