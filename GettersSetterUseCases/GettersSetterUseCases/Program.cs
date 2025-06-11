8c68a1f4151659d890600d8675d8c38c78ffd42f
Faaeq Fazal <faaeqrt@gmail.com>
2025-06-11 12:40:10 +0530

    Implementing getters and setters in c#

---
+++ GettersSetterUseCases/GettersSetterUseCases/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettersSetterUseCases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(400);
+            car.Speed = 5000;
+
+            Console.WriteLine(car.Speed);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Car
+    {
+        private int speed;
+        public Car(int speed) 
+        { 
+            this.speed = speed;
+        }
+
+        public int Speed {  
+            get { return this.speed; } 
+            set { this.speed = value; }
+        }
+    }
+}
