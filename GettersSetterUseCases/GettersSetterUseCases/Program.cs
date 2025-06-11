8ce8faba72d7e6a8e81cd1f39c95ba7692c6864d
Faaeq Fazal <faaeqrt@gmail.com>
2025-06-11 13:38:37 +0530

    Learnt how to use auto implemented properties which is a shortcut when there is no additional logic

--- GettersSetterUseCases/GettersSetterUseCases/Program.cs
+++ GettersSetterUseCases/GettersSetterUseCases/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace GettersSetterUseCases
 {
+    // Auto-implemented property: the compiler creates a private backing field automatically.
+    // Useful for simple get/set scenarios where no additional logic is required.
     internal class Program
     {
         static void Main(string[] args)
@@ -13,7 +15,10 @@ namespace GettersSetterUseCases
             Car car = new Car(400);
             car.Speed = 5000;
 
+            Car scar = new Car("Toyota");
+
             Console.WriteLine(car.Speed);
+            Console.WriteLine(scar.Model);
 
             Console.ReadKey();
         }
@@ -22,14 +27,24 @@ namespace GettersSetterUseCases
     class Car
     {
         private int speed;
+
         public Car(int speed) 
         { 
             this.speed = speed;
+        }
+
+        // Below is the auto implementation property
+        public string Model { get; set; }
+
+        public Car(string model)
+        {
+            this.Model = model;
         }
 
         public int Speed {  
             get { return this.speed; } 
             set { this.speed = value; }
         }
+
     }
 }
